f8327cb7818439df6d6a2449274a7bca95daf393
Ed Weng <edweng@gmail.com>
2021-08-24 22:02:09 -0700

    First commit.

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TestApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TestApp
+
+  target 'TestAppTests' do
+    # Pods for testing
+  end
+
+end
---
+++ TestApp/TestApp.h
@@ -0,0 +1,18 @@
+//
+//  TestApp.h
+//  TestApp
+//
+//  Created by Edward Weng on 8/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestApp.
+FOUNDATION_EXPORT double TestAppVersionNumber;
+
+//! Project version string for TestApp.
+FOUNDATION_EXPORT const unsigned char TestAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestApp/PublicHeader.h>
+
+
